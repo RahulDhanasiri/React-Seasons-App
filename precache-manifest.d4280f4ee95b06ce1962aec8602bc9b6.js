0cb996596cf397e8f8ffe4d6d5f5502cf8e374a0
Rahul Dhanasiri <dhanasirirahul@hotmail.com>
2020-08-15 14:08:36 -0500

    Updates

---
+++ precache-manifest.d4280f4ee95b06ce1962aec8602bc9b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21df258e767411cab359c14400f4bd5a",
+    "url": "/React-Seasons-App/index.html"
+  },
+  {
+    "revision": "23ace69f6f701d3c826c",
+    "url": "/React-Seasons-App/static/css/main.6d1caad8.chunk.css"
+  },
+  {
+    "revision": "c2d246ecc2b83ccac6a6",
+    "url": "/React-Seasons-App/static/js/2.8b77c9df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Seasons-App/static/js/2.8b77c9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23ace69f6f701d3c826c",
+    "url": "/React-Seasons-App/static/js/main.81f9b831.chunk.js"
+  },
+  {
+    "revision": "d61786c062df7bddd04c",
+    "url": "/React-Seasons-App/static/js/runtime-main.b99f6d28.js"
+  }
+]);
